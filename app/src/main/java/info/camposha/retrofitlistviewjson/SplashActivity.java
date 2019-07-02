35032a6cf5bfeb79820c684c9dec4f9f16ab83ff
Iqbal <ibalmaulana9@gmail.com>
2019-07-02 13:16:03 +0700

    All done task 1 i think

---
+++ app/src/main/java/info/camposha/retrofitlistviewjson/SplashActivity.java
@@ -0,0 +1,32 @@
+package info.camposha.retrofitlistviewjson;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally{
+                    startActivity(new Intent (SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+
+            }
+
+        };
+        thread.start();
+    }
+}
